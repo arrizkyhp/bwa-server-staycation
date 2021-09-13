b8453c51a9210be88e39caa1cd0c817f01c3b9ea
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2021-09-13 19:41:27 +0700

    first commit

---
+++ models/Item.js
@@ -0,0 +1,64 @@
+const mongoose = require("mongoose");
+const { ObjectId } = mongoose.Schema;
+
+const itemSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true,
+  },
+  price: {
+    type: Number,
+    required: true,
+  },
+  sumBooking: {
+    type: Number,
+    required: true,
+  },
+  country: {
+    type: String,
+    default: "Indonesia",
+    required: true,
+  },
+  city: {
+    type: String,
+    required: true,
+  },
+  isPopular: {
+    type: Boolean,
+    default: false,
+  },
+  description: {
+    type: String,
+    required: true,
+  },
+  unit: {
+    type: String,
+    required: true,
+  },
+  imageId: [
+    {
+      type: ObjectId,
+      ref: "Image",
+    },
+  ],
+  featureId: [
+    {
+      type: ObjectId,
+      ref: "Feature",
+    },
+  ],
+  activityId: [
+    {
+      type: ObjectId,
+      ref: "Activity",
+    },
+  ],
+  categoryId: [
+    {
+      type: ObjectId,
+      ref: "Category",
+    },
+  ],
+});
+
+module.exports = mongoose.model("Item", itemSchema);
