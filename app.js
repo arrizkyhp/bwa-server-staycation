79c3399ef48a99a56562d2f89db97cd5d774ef17
arrizkyhp <52059831+arrizkyhp@users.noreply.github.com>
2021-09-13 20:09:57 +0700

    deploy heroku

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ const session = require('express-session');
 const flash = require("connect-flash");
 // import mongoose
 const mongoose = require('mongoose');
-mongoose.connect("mongodb://127.0.0.1:27017/db_staycation");
+mongoose.connect("mongodb+srv://arrizkyhp:bwamern@cluster0.5rq9e.mongodb.net/db_staycation?retryWrites=true&w=majority");
 
 
 var indexRouter = require('./routes/index');
